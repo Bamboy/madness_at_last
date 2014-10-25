f1bf593d453e1cf5cc7d836c53c7368deb5716c2
Stephan Ennen <bamboy360@gmail.com>
2014-10-25 14:52:21 -0700

    Added / tweaked GUI. Added main menu to build. Added and (mostly) fixed Timer.

--- Assets/Project/Scripts/GUI/HealthAmmoGUI.cs
+++ Assets/Project/Scripts/GUI/HealthAmmoGUI.cs
@@ -5,15 +5,51 @@ using Stats;
 public class HealthAmmoGUI: MonoBehaviour {
 	private WeaponInventory wi;
 	private PlayerUnit pu;
+	private GunDefinitions wepDic;
+
+	public Texture2D crosshair;
+	[Range(0.0f, 1.0f)] public float crosshairSize;
+	public Vector2 minMaxSize; //X is min, Y is max. Both measured in pixels
+	private float _lastAccuracy = 0.0f;
 	//public GUISkin AmmoSkin;
 	//public GUISkin HealthSkin;
 	void Start(){
-		wi = GameObject.Find("Inventory").GetComponent<WeaponInventory>();
+		wi = GameObject.Find("Inventory").GetComponent<WeaponInventory>(); //TODO - Remove the find!
 		pu = PlayerUnit.instance;
+
+		wepDic = GunDefinitions.Get();
+	}
+	void Update()
+	{
+		//See if our weapon's accuracy has changed.
+		float accuracy = wepDic.GetAccuracy( wi.slots[wi.activeSlot].Weapon );
+		if( accuracy != _lastAccuracy )
+		{
+			//Recalculate our crosshair size.
+			if( accuracy == 0.0f )
+				crosshairSize = 0.0f;
+			else
+				crosshairSize = accuracy / 10.0f;
+		}
+		_lastAccuracy = accuracy;
 	}
 	void OnGUI(){
 		DrawAmmo();
 		DrawHealth();
+		DrawCrosshair();
+	}
+	void DrawCrosshair()
+	{
+		//Calculate our crosshair size based on the weapon's accuracy.
+		int guiSize = Mathf.RoundToInt((crosshairSize * minMaxSize.y) + minMaxSize.x); //X is min, Y is max. Both measured in pixels
+		
+		
+		Rect rect = new Rect(
+			(Screen.width/2) - guiSize/2, 
+			(Screen.height/2) - guiSize/2, 
+			guiSize, 
+			guiSize );
+		GUI.DrawTexture(rect, crosshair, ScaleMode.ScaleToFit);
 	}
 	void DrawHealth(){
 		GUI.Box(new Rect(0, 0, 300, 100), "");
@@ -22,11 +58,14 @@ public class HealthAmmoGUI: MonoBehaviour {
 	void DrawAmmo(){
 		//GUI.skin = AmmoSkin;
 		GUI.BeginGroup(new Rect(Screen.width - 150f, Screen.height - 150f, 150, 150), "");
-		GUI.Box(new Rect(0, 0, 150, 150), "");
-		GUI.Label(new Rect(5, 130, 50, 25), "Ammo:");
-		GUI.Label(new Rect(55, 130, 70, 25), wi.GetClipAmmoFor(wi.slots[wi.activeSlot].Weapon).ToString("n0"));
-		GUI.Label(new Rect(65, 130, 70, 25), "Clip:");
-		GUI.Label(new Rect(95, 130, 70, 25), wi.GetAmmoFor(wi.slots[wi.activeSlot].Weapon).ToString("n0"));
+			GUI.Box(new Rect(0, 0, 150, 150), "");
+			GUI.Label(new Rect(5, 115, 100, 75), wi.slots[wi.activeSlot].Weapon);
+			GUI.Label(new Rect(5, 130, 50, 25), "Ammo:");
+			GUI.Label(new Rect(55, 130, 70, 25), wi.GetClipAmmoFor(wi.slots[wi.activeSlot].Weapon).ToString("n0"));
+			GUI.Label(new Rect(65, 130, 70, 25), "Clip:");
+			GUI.Label(new Rect(95, 130, 70, 25), wi.GetAmmoFor(wi.slots[wi.activeSlot].Weapon).ToString("n0"));
 		GUI.EndGroup();
 	}
+
+
 }
--- Assets/Project/Scripts/GUI/Timer.cs
+++ Assets/Project/Scripts/GUI/Timer.cs
@@ -4,49 +4,65 @@ using System.Collections;
 public class Timer : MonoBehaviour {
 	public Texture2D SecondsHand;
 	public Texture2D MinutesHand;
+	public bool CanCount = true;
 	public bool CountDown = false;
 	public float _time;
 	public float _startingTime;
 	public float _countFromTime;
 	public float _countToTime;
-	public bool CanCount = true;
-	public int seconds = 0;
-	public int minutes = 0;
+	public bool SmoothGUI = true;
+	public float seconds = 0;
+	public float minutes = 0;
 	public float _SAngle = -180.0f;
 	public float _MAngle = -180.0f;
-	Rect SecondsRect = new Rect(200, 200, 5, 50);
-	Rect MinuteRect = new Rect(200, 200, 5, 70);
-	Vector2 pivot = new Vector2(202.5f, 200);
+
+	//public Vector2 Position = new Vector2(200, 200);
+	//public Vector2 PivotOffset = new Vector2(2.5f, 0.0f);
+	//public Vector2 SecondsSize = new Vector2(5, 70);
+	//public Vector2 MinutesSize = new Vector2(5, 50);
+
+	Rect SecondsRect = new Rect(200, 200, 5, 70);
+	Rect MinuteRect = new Rect(200, 200, 5, 50);
+	Vector2 pivot = new Vector2(2.5f, 0.0f); //Vector2(202.5f, 200);
 
 	void Start(){
 		_startingTime = Time.time;
+
+		SecondsRect = new Rect( Screen.width - 80, 80, 5, 70 );
+		MinuteRect = new Rect( Screen.width - 80, 80, 5, 50 );
+		pivot = new Vector2( pivot.x + Screen.width - 80, pivot.y + 80 );
 	}
-	void Update(){
+	void Update()
+	{
 		if(CanCount){
 			if(CountDown){
 				_time = _countFromTime - Time.time - _startingTime;
-				seconds = (int)_time % 60;
-				minutes = (int)_time / 60;
+				seconds = SmoothGUI ? _time % 60.0f : Mathf.Ceil(_time % 60);
+				minutes = SmoothGUI ? _time / 60.0f : Mathf.Ceil(_time / 60.0f);
 				
-				_SAngle = 6*seconds + 180;
-				_MAngle = 6*minutes + 180;
+				_SAngle = (6.0f * seconds) + 180.0f;
+				_MAngle = (6.0f * minutes) + 180.0f;
 			} else {
 				_time = Time.time - _startingTime;
-				seconds = (int)_time % 60;
-				minutes = (int)_time / 60;
+				seconds = SmoothGUI ? _time % 60.0f : Mathf.Ceil(_time % 60);
+				minutes = SmoothGUI ? _time / 60.0f : Mathf.Ceil(_time / 60.0f);
 				
-				_SAngle = 6*seconds - 180;
-				_MAngle = 6*minutes - 180;
+				_SAngle = (6.0f * seconds) - 180.0f;
+				_MAngle = (6.0f * minutes) - 180.0f;
 			}
 		}
-		if(_time <= 0){
+		/*
+		if(_time <= 0.0f){
 			CanCount = false;
-			_time = 0;
+			_time = 0.0f;
+			Debug.Log("1 _Time: "+ _time, this);
 		} else if(_time >= _countToTime){
 			CanCount = false;
+			Debug.Log("2; _Time: "+ _time +", _ToTime: "+ _countToTime, this);
 		} else {
 			CanCount = true;
-		}
+		} */
+
 	}
 	void OnGUI(){
 		Seconds();
