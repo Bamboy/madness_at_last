84b870d6109bd49233dcd2ea6f6b397c993326b4
Stephan Ennen <bamboy360@gmail.com>
2014-10-28 13:50:03 -0700

    Created AI spawning system. Could use improvements.

--- Assets/Project/Scripts/Main.cs
+++ Assets/Project/Scripts/Main.cs
@@ -3,6 +3,47 @@ using System.IO;
 
 public class Main : MonoBehaviour
 {
+	#region Global Access
+	private static Main main;
+	public static Main Get() //This makes it so we can remotely access this script from anywhere in the scene.
+	{
+		if( !main ) //Check if we already have an instance of this script in the scene, if so just return it.
+		{
+			main = GameObject.FindObjectOfType< Main >(); //Find this script in the scene.
+			if( !main ) //Make sure we found something.
+			{
+				//Make a new object and attach this script to it. 
+				GameObject newObj = new GameObject("=== Main Controller ===");
+				main = newObj.AddComponent< Main >();
+			}
+		}
+		return main;
+	}
+	#endregion
+
+
+	private static Transform player;
+	public Transform Player { get{return player;} set{player = value;} }
+	void FindPlayerInScene()
+	{
+		GameObject ply = GameObject.FindGameObjectWithTag( "Player" );
+		if( ply == null )
+		{
+			Player = null;
+			Debug.LogWarning( "Player could not be found in the scene!", this );
+		}
+		else
+			Player = ply.transform;
+	}
+
+	
+	void OnLevelWasLoaded( int id )
+	{
+		FindPlayerInScene();
+	}
+
+
+
 	void Awake()
 	{
 		DontDestroyOnLoad(this.gameObject);
@@ -12,6 +53,7 @@ public class Main : MonoBehaviour
 		} else {
 			Utils.KeyManager.SetDefaultKeys();
 		}
+		FindPlayerInScene();
 	}
 
 	void OnApplicationQuit()
