df7414cdc94edc72cf60471568e60de67445b21d
Stephan Ennen <bamboy360@gmail.com>
2014-11-13 15:57:39 -0800

    All weapons now added to the new weapon system. Things need to be re-integrated. Also... RPG :D

--- Assets/Project/Scripts/Backend/Weapons/New WeaponSystem/Inventory/GunInventory.cs
+++ Assets/Project/Scripts/Backend/Weapons/New WeaponSystem/Inventory/GunInventory.cs
@@ -5,7 +5,7 @@ using Excelsion.WeaponSystem.Weapons;
 
 ///////////////////////////////////
 /// By: Stephan "Bamboy" Ennen ////
-/// Last Updated: 11/09/14     ////
+/// Last Updated: 11/13/14     ////
 ///////////////////////////////////
 
 namespace Excelsion.WeaponSystem
@@ -15,6 +15,7 @@ namespace Excelsion.WeaponSystem
 		public GunBase[] guns; //This contains one of each weapon we will ever be able to use. This also contains information about our ammo.
 		public bool[] hasWeapon; //1 means we have the weapon in the slot, 0 means we don't.
 		public int activeWeapon = 0;
+		private int weaponCount;
 
 		#region Accessors
 		public int ActiveWeapon {
@@ -68,15 +69,94 @@ namespace Excelsion.WeaponSystem
 				hasWeapon = new bool[guns.Length];
 				activeWeapon = -1;
 			}
+			CountWeapons();
 			#endregion
+		}
+		int CountWeapons()
+		{
+			weaponCount = 0;
+			for( int i = 0; i < guns.Length; i++ )
+			{ 
+				if( hasWeapon[i] == false )
+				{
+					Debug.Log ("We dont have this weapon");
+					continue; //Continue stops this current loop cycle and goes to the next one.
+				}
+				else if( guns[i].Ammo > 0 || guns[i].ClipAmmo > 0 ) 
+				{
+					Debug.Log ("We do not have ammo for this weapon");
+					continue;
+				}
+				else
+				{
+					Debug.Log ("We do have this weapon");
+					weaponCount++;
+				}
+			}
+			Debug.Log( weaponCount );
+			return weaponCount;
 		}
 
 		#region Inputs
-		public bool InputFire{set{ guns[activeWeapon].InputFire = value; }}
+		public bool InputFire   {set{ guns[activeWeapon].InputFire = value; }}
 		public bool InputReload {set{ guns[activeWeapon].InputReload = value; }}
 
+		#region Weapon Selection
+		public void NextWeapon()
+		{
+			if( weaponCount > 1 ) //Only enable switching if we have 2 or more weapons.
+			{
+				int newWep = ActiveWeapon;
+				newWep++;
+				if( newWep == guns.Length ) 
+					newWep = 0; //We would get an Index Out Of Range error if we didn't change it!
 
+				int breakCounter = 0;
+				while((guns[newWep].Ammo <= 0 && guns[newWep].ClipAmmo <= 0) || hasWeapon[newWep] == false ) //Don't stop on a weapon unless we have it!
+				{
+					newWep++;
+					if( newWep == guns.Length ) 
+						newWep = 0; //We would get an Index Out Of Range error if we didn't change it!
 
+					breakCounter++;
+					if( breakCounter > 100 )
+					{
+						Debug.LogWarning("Infinite loop detected!", this);
+						break;
+					}
+
+				}
+				ActiveWeapon = newWep; //Finalize our weapon switch.
+			}
+		}
+		public void PreviousWeapon()
+		{
+			if( weaponCount > 1 ) //Only enable switching if we have 2 or more weapons.
+			{
+				int newWep = ActiveWeapon;
+				newWep--;
+				if( newWep == -1 ) 
+					newWep = guns.Length - 1; //We would get an Index Out Of Range error if we didn't change it!
+				
+				int breakCounter = 0;
+				while((guns[newWep].Ammo <= 0 && guns[newWep].ClipAmmo <= 0) || hasWeapon[newWep] == false ) //Don't stop on a weapon unless we have it!
+				{
+					newWep--;
+					if( newWep == -1 ) 
+						newWep = guns.Length - 1; //We would get an Index Out Of Range error if we didn't change it!
+					
+					breakCounter++;
+					if( breakCounter > 100 )
+					{
+						Debug.LogWarning("Infinite loop detected!", this);
+						break;
+					}
+					
+				}
+				ActiveWeapon = newWep; //Finalize our weapon switch.
+			}
+		}
+		#endregion
 
 		#endregion
 
---
+++ Assets/Project/Scripts/Backend/Weapons/New WeaponSystem/Projectiles/RPGRocket.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using Stats;
+
+public class RPGRocket : MonoBehaviour 
+{
+	public DamageType damageType;
+	public float maxDamage; //Damage will function as a 'falloff', so this is the maximum damage possible
+	public float forceRequired = 1.0f; //The collision force needed before an explosion will happen.
+	public float explosionRadius = 7.5f;
+	public float armTime = 0.25f; //RPG will NEVER explode before this time.
+	public float thrust = 5.0f; //Accelleration
+
+	public ParticleSystem particles;
+
+	private bool armed;
+	// Use this for initialization
+	void Awake () 
+	{
+		rigidbody.useGravity = true;
+		Invoke("AutoCleanup", 30.0f);
+		Invoke("Arm", armTime);
+	}
+
+	void Arm()
+	{
+		rigidbody.useGravity = false;
+		armed = true;
+		particles.Play( true );
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if( armed )
+		{
+			rigidbody.AddRelativeForce( 0,0,thrust, ForceMode.Acceleration );
+		}
+	}
+
+	void OnCollisionEnter( Collision collision )
+	{
+		if( collision.relativeVelocity.magnitude > forceRequired )
+		{
+			Debug.Log( collision.relativeVelocity );
+			Explode ();
+		}
+	}
+
+	void Explode()
+	{
+		particles.enableEmission = false;
+		particles.GetComponentInChildren< ParticleSystem >().enableEmission = false;
+		particles.transform.parent = null;
+		DestroyObject( particles.gameObject, 6.0f );
+
+
+		AutoCleanup();
+	}
+
+	void AutoCleanup()
+	{ 
+		DestroyObject( this.gameObject ); 
+	}
+}
